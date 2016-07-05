68ca5df3b34f7c7197e972fb204a10af47609cc0
EdG570 <cole570@hotmail.com>
2016-07-05 17:08:35 -0400

    Finished Tic Tac Toe. 0 offenses in Rubocop.

--- ttt_game.rb
+++ ttt_game.rb
@@ -1,8 +1,21 @@
+require 'pry'
+
+INITIAL_MARKER = ' '.freeze
+PLAYER_MARKER = 'X'.freeze
+COMPUTER_MARKER = 'O'.freeze
+WINNING_LINES = [[1, 2, 3], [4, 5, 6],
+                 [7, 8, 9], [1, 4, 7],
+                 [2, 5, 8], [3, 6, 9],
+                 [1, 5, 9], [3, 5, 7]].freeze
+
 def prompt(msg)
   puts "=> #{msg}"
 end
 
+# rubocop:disable Metrics/AbcSize
 def display_board(brd)
+  system "clear"
+  puts "You're an #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}"
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -17,46 +30,77 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
+# rubocop:enable Metrics/AbcSize
 
 def initialize_board
   new_board = {}
-  (1..9).each { |num| new_board[num] = ' ' }
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
 end
 
 def empty_squares(brd)
-  brd.keys.select { |num| brd[num] == ' '}
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
 end
 
 def player_places_piece!(brd)
   user_choice = ''
   loop do
-    prompt("Pick a cell: 1 thru 9")
+    prompt("Pick a cell: (#{empty_squares(brd).join(', ')})")
     user_choice = gets.chomp.to_i
-    if empty_squares(brd).include?(user_choice)
-      break
-    else
-      prompt("Sorry, that's not a valid choice.")
-    end
+    break if empty_squares(brd).include?(user_choice)
+    prompt("Sorry, that's not a valid choice.")
   end
 
-  brd[user_choice] = 'X'
+  brd[user_choice] = PLAYER_MARKER
 end
 
-board = initialize_board
-display_board(board)
+def computer_places_piece!(brd)
+  comp_choice = empty_squares(brd).sample
+  brd[comp_choice] = COMPUTER_MARKER
+end
 
-player_places_piece!(board)
-display_board(board)
-puts board.inspect
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
 
-#computer_choice = rand(1..9)
-# Computer picks a cell
-# Display updated board
-# If 3 in a row horizontally, vertically or diagnally display winner
-# Elsif board is full display tie
-# => Ask user if they want to play again
-# =>   If yes loop back to display empty board
-# =>   If no say "Good bye!"
-# Else loop back to ask user to pick a cell (line 2)
+def winner?(brd)
+  !!detect_winner(brd)
+end
 
+def detect_winner(brd)
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return 'Computer'
+    end
+  end
+  nil
+end
+
+loop do
+  board = initialize_board
+  display_board(board)
+
+  loop do
+    player_places_piece!(board)
+    display_board(board)
+    break if board_full?(board) || winner?(board)
+
+    computer_places_piece!(board)
+    display_board(board)
+    break if board_full?(board) || winner?(board)
+  end
+
+  if winner?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "The game has ended in a tie!"
+  end
+
+  prompt "Would you like to play again? (y or n)"
+  user_play = gets.chomp.downcase
+  break if user_play != 'y'
+end
+
+prompt "Thank you for playing Tic Tac Toe. Good bye!"
